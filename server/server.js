96cdeb05a088c69129740c27e72d595316c9e6ee
DanishDude <christian.hinge@gmail.com>
2020-11-09 13:43:31 +0100

    helmet config

--- server/server.js
+++ server/server.js
@@ -26,12 +26,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 require('./db');
-app.use(helmet());
 app.use(helmet.contentSecurityPolicy({
   directives: {
+    defaultSrc: ["'self"],
     scriptSrc: ["'self'", "https://mypolitic.herokuapp.com/"]
   }
 }));
+console.log('HELMET: ',helmet.contentSecurityPolicy.getDefaultDirectives());
 app.use(cookieParser());
 app.use(cors());
 app.use(bearerToken());
