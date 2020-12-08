7d7028ebbf7e6046f0c5c7a147d4ad095a364a19
DanishDude <christian.hinge@gmail.com>
2020-12-08 15:42:34 +0100

    rm unused info

--- client/src/containers/AboutUs/AboutUs.jsx
+++ client/src/containers/AboutUs/AboutUs.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 const AboutUs = () => {
-  return (
-    <div className="AboutUs">
-      <h1>About Us Page</h1>
-      <h3>{ process.env.REACT_APP_API_SERVER }</h3>
-    </div>
-  );
+    return (
+        <div className="AboutUs">
+            <h1>About Us Page</h1>
+        </div>
+    );
 };
 
 export default AboutUs;
